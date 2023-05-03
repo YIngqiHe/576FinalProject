9cf3d0d3f94552fc1dad54dbd8ef7c0235ae6ae3
yininghyq <love332@qq.com>
2023-05-03 08:52:18 -0700

    Add scene and subshots

--- org/wikijava/sound/playWave/ShotDetector.java
+++ org/wikijava/sound/playWave/ShotDetector.java
@@ -56,6 +56,18 @@ public class ShotDetector {
             this.shots.add(shot);
         }
 
+        public void addSubshot(Subshot subshot) {
+            Shot shot = this.shots.get(shots.size() - 1);
+            String start_time = shot.getStartTime();
+            int frame_id = shot.getFrameId();
+            if (shot.subshots.size() == 0) {
+               Subshot firstSubshot = new Subshot(start_time, frame_id);
+               this.shots.get(shots.size() - 1).subshots.add(firstSubshot);
+               
+            }
+            this.shots.get(shots.size() - 1).subshots.add(subshot);
+        }
+
         public String getStartTime() {
             return startTime;
         }
@@ -147,6 +159,11 @@ public class ShotDetector {
             this.frameId = 0;
         }
 
+        public Subshot(String startTime, int frameId) {
+            this.startTime = startTime; 
+            this.frameId = frameId;
+        }
+
         public String getStartTime() {
             return startTime;
         }
@@ -165,8 +182,38 @@ public class ShotDetector {
 
     }
 
+    
+    public Mat getBackgroundMask(Mat frame1, Mat frame2) {
 
-    public double getHistResult(Mat frame1, Mat frame2) {
+
+        // Convert frames to grayscale
+        Mat gray1 = new Mat();
+        Mat gray2 = new Mat();
+        Imgproc.cvtColor(frame1, gray1, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.cvtColor(frame2, gray2, Imgproc.COLOR_BGR2GRAY);
+
+        // Compute absolute difference between frames
+        Mat diff = new Mat();
+        Core.absdiff(gray1, gray2, diff);
+
+        // Apply threshold to get foreground mask
+        Mat mask = new Mat();
+        Imgproc.threshold(diff, mask, 30, 255, Imgproc.THRESH_BINARY);
+
+        // Apply morphology operations to remove noise and fill in gaps
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(3, 3));
+        Imgproc.morphologyEx(mask, mask, Imgproc.MORPH_OPEN, kernel);
+        Imgproc.morphologyEx(mask, mask, Imgproc.MORPH_CLOSE, kernel);
+
+        // Invert mask to get background mask
+        Mat bgMask = new Mat();
+        Core.bitwise_not(mask, bgMask);
+
+        return bgMask;
+    }
+    
+
+    public double getHistResult(Mat frame1, Mat frame2, Mat mask1, Mat mask2) {
 
         Mat hsvLastFrame = new Mat();
         Mat hsvCurrentFrame = new Mat();
@@ -182,8 +229,8 @@ public class ShotDetector {
         Mat hist1 = new Mat();
         Mat hist2 = new Mat();
  
-        Imgproc.calcHist(Arrays.asList(hsvLastFrame), channels, mask, hist1, histSize, histRanges, false);
-        Imgproc.calcHist(Arrays.asList(hsvCurrentFrame), channels, mask, hist2, histSize, histRanges, false);
+        Imgproc.calcHist(Arrays.asList(hsvLastFrame), channels, mask1, hist1, histSize, histRanges, false);
+        Imgproc.calcHist(Arrays.asList(hsvCurrentFrame), channels, mask2, hist2, histSize, histRanges, false);
 
         Core.normalize(hist1, hist1, 0, 1, Core.NORM_MINMAX);
         Core.normalize(hist2, hist2, 0, 1, Core.NORM_MINMAX);
@@ -250,7 +297,7 @@ public class ShotDetector {
 
         sceneList.add(scene0);
   
-        VideoCapture cap = new VideoCapture("InputVideo.mp4");
+        VideoCapture cap = new VideoCapture("InputVideo1.mp4");
         if (!cap.isOpened()) {
             System.out.println("Error opening video file.");
             return;
@@ -258,7 +305,6 @@ public class ShotDetector {
 
         Mat previousFrame = new Mat();
         cap.read(previousFrame);
-
 
         Mat previousGray = new Mat();
         Imgproc.cvtColor(previousFrame, previousGray, Imgproc.COLOR_BGR2GRAY);
@@ -274,6 +320,12 @@ public class ShotDetector {
 
         Mat lastFrame = currentFrame.clone();
         Mat lastTwoFrame = lastFrame.clone();
+        Mat lastSceneFrame = lastFrame.clone();
+
+        int mask_status = 0;
+        Mat frameMask1 = new Mat();
+        Mat frameMask2 = new Mat();
+        Mat frameMask3 = new Mat();
 
         while (cap.read(currentFrame)) {
 
@@ -284,30 +336,67 @@ public class ShotDetector {
             String formattedTime = dateFormat.format(new Date(frameTimestamp));
 
             double threshold = 40.0;
+
+
+            // if (mask_status == 1) {
+            //     lastFrame = lastTwoFrame.clone();
+            //     lastTwoFrame = previousFrame.clone();
+            //     mask_status++;
+            //     // System.out.println(frameIndex);
+            // } else if (mask_status > 1 && mask_status < 5) {
+            //     mask_status++;
+            // } else if (mask_status == 5) {
+            // // if (mask_status == 1) {
+            // //     lastFrame = lastTwoFrame.clone();
+            // //     lastTwoFrame = previousFrame.clone();
+            //     frameMask2 = getBackgroundMask(lastTwoFrame, currentFrame);
+            //     frameMask3 = getBackgroundMask(lastTwoFrame, lastSceneFrame);
+            //     double result1 = getHistResult(lastFrame, lastTwoFrame, frameMask1, frameMask2);
+            //     double result2 = getHistResult(lastSceneFrame, lastTwoFrame, frameMask3, frameMask2);
+            //     frameMask1 = frameMask2.clone();
+            //     mask_status = 0;
+            //     System.out.println("HistResult: " + result1 + ".  Result2: " + result2);
+            //     if (result1 > 10 && result2> 50) {
+            //         lastSceneFrame = lastTwoFrame.clone();
+            //         System.out.println("Scene change detected! Frame: " + frameIndex + ". Current Time: " 
+            //             + formattedTime + ". HistResult: " + result1);
+            //     }
+
+            // }
+            
             if (!isSameShot(previousFrame, currentFrame, threshold) && frameIndex - lastIndex > 15) {
-                // System.out.println("Shot change detected! Frame: " + frameIndex + ". Current Time: " + formattedTime);
+                System.out.println("Shot change detected! Frame: " + frameIndex + ". Current Time: " + formattedTime);
                 shotIndexes.add(frameIndex);
-                lastIndex = frameIndex;
+               
                 
-                double result1 = getHistResult(lastFrame, currentFrame);
-                double result2 = getHistResult(lastTwoFrame, currentFrame);
-                double result3 = getHistResult(previousFrame, currentFrame);
+                double result1 = getHistResult(lastFrame, currentFrame, new Mat(), new Mat());
+                double result2 = getHistResult(lastTwoFrame, currentFrame, new Mat(), new Mat());
+                double result3 = getHistResult(previousFrame, currentFrame, new Mat(), new Mat());
 
                 
-                // System.out.println("Chi-square distance between the two histograms: " + result1 + ". Result2: " + result2);
+                // System.out.println("Result1: " + result1 + ". Result2: " + result2 + ". Result3: " + result3);
 
                 Shot shot = new Shot(formattedTime, frameIndex);
-                if (result1 > 10 && result2 > 10 && result3 > 10 && result1+ result2 +result3 > 300) {
-                    // System.out.println("Scene changed! Result1:" + result1 + ". Result2:" + result2);
+                if (result1 > 40 && result2 > 40 && result3 > 40) {
+                    System.out.println("Scene changed! Result1:" + result2 + ". Result2:" + result3);
                     lastTwoFrame = lastFrame.clone();
                     Scene scene = new Scene(formattedTime, frameIndex);
                     scene.addShot(shot);
                     sceneList.add(scene);
                 } else {
-                    sceneList.get(sceneList.size() - 1).addShot(shot);;
+                    if (frameIndex - lastIndex <= 30) {
+                        Subshot subshot = new Subshot(formattedTime, frameIndex);
+                        sceneList.get(sceneList.size() - 1).addSubshot(subshot);
+                    } else {
+                        sceneList.get(sceneList.size() - 1).addShot(shot);
+                    }
+                    
                 }
-                // lastTwoFrame = lastFrame.clone();
+
+                lastIndex = frameIndex;
+                lastTwoFrame = lastFrame.clone();
                 lastFrame = currentFrame.clone();
+                mask_status++;
 
             }
 
